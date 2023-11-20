7ec2eceff404463ec3d54284b9cd4c24e8dcfc88
Rodríguez Garrucho, Oscar <orodriguezg@eservicios.indra.es>
2023-11-20 23:56:04 +0100

    Initial Commit

---
+++ Apptonomo/ApptonomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApptonomoApp.swift
+//  Apptonomo
+//
+//  Created by Fran Alarza on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ApptonomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
